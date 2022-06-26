a912bc20ea16cc67da8dece3ec48d25f422c5435
Jack Sun <jsun1590@gmail.com>
2022-06-26 15:47:28 +0800

    dockerize backend

---
+++ docker/server/runtime.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "████ ${APP_NAME^^} - DJANGO CONTAINER STARTING... ████████████████████████████████████"
+figlet $APP_NAME
+
+# Debug / Sanity check info
+echo "Current Dir / Files (Debug)" | boxes -d shell -p a1l2
+pwd
+ls -al
+
+printf "\n" && echo "Python Path & Version (Debug)" | boxes -d shell -p a1l2
+which python
+python -V
+
+
+# Setup everytime it runs
+
+# Migrate database
+printf "\n" && echo "Running django database migrations" | boxes -d shell -p a1l2
+python manage.py migrate --noinput
+
+# Create Django Superuser
+printf "\n" && echo "Creating Django Superuser" | boxes -d shell -p a1l2
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+
+printf "\n" && echo "MIGRATION & SETUP TASK COMPLETED" | boxes -d dog -a c
+
+# Run inbuilt Django server if ENV is development
+if [ "${APP_ENV^^}" = "DEVELOPMENT" ]; then
+
+    # Install extra non-prod packages
+    printf "\n" && echo "Installing dev dependencies for $APP_ENV" | boxes -d shell -p a1l2
+    poetry install
+
+    # Run developments
+    printf "\n" && echo "Starting inbuilt django webserver" | boxes -d shell -p a1l2
+    echo "Running: python manage.py runserver 0.0.0.0:8081"
+    python manage.py runserver 0.0.0.0:8081
+    exit
+fi
+
+# ===================
+# Run Django/Gunicorn
+# ===================
+if [ "${APP_ENV^^}" = "PRODUCTION" ]; then
+
+    # Run Gunicorn / Django
+    printf "\n" && echo " Running Gunicorn / Django" | boxes -d shell -p a1l2
+    echo "Running: gunicorn api.wsgi -b 0.0.0.0:8081 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50"
+    # gunicorn api.wsgi -b 0.0.0.0:8081 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50
+    gunicorn api.wsgi -b 0.0.0.0:8081
+fi
--- server/api/settings.py
+++ server/api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -27,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,9 +77,13 @@ WSGI_APPLICATION = "api.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
