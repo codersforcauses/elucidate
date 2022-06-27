39c049b2ff30290e80f9ec51a796a7a91f75a2ad
Jack Sun <jsun1590@gmail.com>
2022-06-27 15:55:42 +0000

    add ASG colors

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: '#53DFCB',
+        blue: '#7087FF',
+        orange: '#FCD47C',
+        red: '#F54A87',
+      },
+    },
+  },
+  plugins: [],
+};
