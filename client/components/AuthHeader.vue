51b6903ed758d43623c610ce599f2cc80ec3ca01
Carmen <carmen020824@gmail.com>
2022-06-30 10:48:14 +0000

    add header & footer for signup page

---
+++ client/components/AuthHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center my-5">
+    <img class="w-11" src="~/assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthHeader',
+};
+</script>
