3bfd2fc3b3abda98a48a28a25d0e8cd392038ed7
jsun1590 <jsun1590@gmail.com>
2022-06-30 11:25:33 +0000

    add fontawesome icons

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,10 +20,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/fontawesome.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -49,4 +49,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   devServerHandlers: [],
+
+  // Purge CSS Configuration: https://go.nuxtjs.dev/config-purgecss
+  purgeCSS: {
+    whitelistPatterns: [/svg.*/, /fa.*/],
+  },
 };
