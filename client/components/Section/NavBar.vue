3984f28bd1e8a959da3e6fec2730c71c70a1252b
jsun1590 <jsun1590@gmail.com>
2022-06-30 11:26:14 +0000

    style navbar

--- client/components/Section/NavBar.vue
+++ client/components/Section/NavBar.vue
@@ -1,12 +1,24 @@
 <template>
   <nav class="h-[78px] flex items-center shadow-xl justify-between">
     <div>
-      <NuxtLink to="/" class="font-Montserrat mx-4">Home</NuxtLink>
-      <NuxtLink to="/quiz" class="font-Montserrat mx-4">Quiz</NuxtLink>
-      <NuxtLink to="/about" class="font-Montserrat mx-4">About</NuxtLink>
+      <NuxtLink to="/" class="mx-4 font-Montserrat">Home</NuxtLink>
+      <NuxtLink to="/quiz" class="mx-4 font-Montserrat">Quiz</NuxtLink>
+      <NuxtLink to="/about" class="mx-4 font-Montserrat">About</NuxtLink>
     </div>
-    <div>
-      <input type="text" class="bg-gray-200 h-10 rounded-md" />
+    <div class="flex items-center">
+      <div
+        class="flex items-center h-10 bg-gray-200 rounded-md focus-within:outline focus-within:outline-2 focus-within:outline-gray-400"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'magnifying-glass']"
+          class="mx-4 text-gray-400"
+        />
+        <input
+          type="text"
+          placeholder="Find a quiz"
+          class="bg-transparent focus:outline-none"
+        />
+      </div>
       <ButtonElement>Log in</ButtonElement>
       <ButtonElement>Sign up</ButtonElement>
     </div>
