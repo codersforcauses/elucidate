85e61bb56bcf28b32734337b6b91a39c4d3a9300
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-02 08:25:08 +0000

    added colours, font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        primary: '#53DFCB',
-        blue: '#7087FF',
-        orange: '#FCD47C',
-        red: '#F54A87',
-      },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#53DFCB',
+      red: '#F54A87',
+      orange: '#FCD47C',
+      yellow: '#FCD47C',
+      green: '#53DFCB',
+      blue: '#7087FF',
+      white: '#FFFFFF',
+    },
+
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
     },
   },
   plugins: [],
