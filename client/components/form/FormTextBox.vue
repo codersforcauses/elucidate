94807efcbc674d9ec1f6067015f45936f2ee29b4
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-02 20:09:20 +0800

    Add TextBox component

---
+++ client/components/form/FormTextBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-col mb-2">
+    <h2 class="font-bold mb-0.5">{{ header }}</h2>
+    <input :type="inputType" class="drop-shadow h-10 w-full" />
+    <p v-if="subtext !== null && subtextLink === null">
+      {{ subtext }}
+    </p>
+    <a
+      v-else-if="subtextLink !== null"
+      :href="subtextLink"
+      class="place-self-end text-xs text-white underline underline-offset-2 pb-10 pt-1"
+    >
+      {{ subtext }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextBox',
+  props: {
+    header: String,
+    subtext: {
+      type: [String, null],
+      optional: true,
+      default: null,
+    },
+    subtextLink: {
+      type: [String, null],
+      optional: true,
+      default: null,
+    },
+    inputType: {
+      type: String,
+      default: 'text',
+      validator: function (value) {
+        return ['text', 'password'].includes(value);
+      },
+    },
+  },
+};
+</script>
--- client/pages/login-page.vue
+++ client/pages/login-page.vue
@@ -1,109 +1,45 @@
 <template>
-<div>
-    
+  <div>
     <div class="flex flex-grow flex-col items-center content-center">
-        <AuthHeader/>
-        <h1 class="italic text-3xl my-4">Sign In</h1>
+      <AuthHeader />
+      <h1 class="italic text-3xl my-4">Sign In</h1>
 
-        <div class="flex flex-col bg-green/75 py-6 px-10 rounded drop-shadow-lg mb-10">
-            <h2 class="font-bold mb-0.5">Email</h2><input type="text" class="drop-shadow h-10 w-full">
+      <div
+        class="flex flex-col bg-green/75 py-6 px-10 rounded drop-shadow-lg mb-10"
+      >
+        <TextBox header="Email" inputType="text" />
+        <TextBox
+          header="Password"
+          inputType="password"
+          subtext="forgot password?"
+          subtextLink="forgot-password"
+        />
 
-            <h2 class="font-bold mb-0.5 mt-2">Password</h2><input type="password" class="drop-shadow h-10 w-full">
-            <a href="forgot-password" class="place-self-end text-xs text-white underline underline-offset-2 pb-10 pt-1">forgot password?</a>
-
-            <button class="font-bold text-red bg-white w-20 rounded border-10 border-red border-solid">Login</button>
-            <p>don't have an account? <a href="sign-up"></a>sign up!</a></p>
-            </div>
-        </div>
-        <AuthFooter/>
+        <button
+          class="font-bold text-red bg-white w-20 rounded border-10 border-red border-solid"
+        >
+          Login
+        </button>
+        <p>
+          don't have an account?
+          <a href="sign-up">sign up!</a>
+        </p>
+      </div>
+      <AuthFooter />
     </div>
-   
-</div>  
+  </div>
 </template>
 
 <script>
-import AuthFooter from '~/components/AuthFooter.vue'
-import AuthHeader from '~/components/AuthHeader.vue'
+import AuthFooter from '~/components/AuthFooter.vue';
+import AuthHeader from '~/components/AuthHeader.vue';
+import TextBox from '~/components/form/FormTextBox.vue';
 
 export default {
-    components: {
-        AuthFooter,
-        AuthHeader
-    },
-}
+  components: {
+    AuthFooter,
+    AuthHeader,
+    TextBox,
+  },
+};
 </script>
-
-<!--
-<style>
-
-div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-family: 'Montserrat';
-
-}
-
-#logo {
-    height: 50px;
-    width: auto;
-    margin: 15px;
-}
-
-.sign-in {
-    font-size: 150%;
-    margin: 0 0 15px 0;
-}
-
-#box {
-    background-color: #53DFCB73;
-    height: 200px;
-    width: 200px;
-    padding: 10px;
-}
-
-h2 {
-    text-align: left;
-    font-size: 80%;
-    font-weight: bold;
-
-}
-
-#password {
-    font-size: 20%;
-    color: white;
-    margin: 0 0 5px 0;
-    text-align: right;
-}
-
-#login {
-    background-color: white;
-    color: #F54A87;
-    border: 1px solid #F54A87;
-    border-radius: 3px;
-    font-size: 80%;
-    font-weight: bold;
-    padding: 1px 10px 1px 10px;
-    margin: 0 0 10px 0;
-}
-
-p,
-#sign-up {
-    display: flex;
-    flex-direction: row;
-    font-size: 50%;
-    justify-content: space-evenly;
-}
-
-p {
-    margin: 0 0 0 5px;
-}
-
-#sign-up{
-    color: #7087FF;
-}
-
-</style>
-
--->
