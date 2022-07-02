3e1845a58ed11ae01e659752ff0efd66b2bc87b8
Shalini Mohan <mohan.shalini@outlook.com>
2022-07-02 13:17:59 +0800

    Add global styling

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        colors: {
+           'blue': '#7087FF',
+           'green': '#53DFCB',
+           'yellow': '#FCD47C',
+           'red': '#F54A87', 
+        },
+
+        fontFamily: {
+            sans: ['Montserrat', 'sans-serif'],
+        }
+    }
+}
