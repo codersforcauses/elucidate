907bdcc1b9bce60588104fedc2051f528379a9d2
jsun1590 <jsun1590@gmail.com>
2022-07-02 10:54:49 +0000

    style button

---
+++ client/components/Input/ButtonElement.vue
@@ -0,0 +1,36 @@
+<template>
+  <span>
+    <button
+      v-if="type === 'primary'"
+      :class="'text-white bg-primary ' + commonCSS"
+    >
+      <slot></slot>
+    </button>
+
+    <button
+      v-else
+      :class="
+        'bg-white text-primary border border-solid border-primary ' + commonCSS
+      "
+    >
+      <slot></slot>
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ButtonElement',
+  data: () => ({
+    commonCSS: 'h-10 w-32 rounded-md',
+  }),
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+    },
+  },
+};
+</script>
+
+<style></style>
