74d293e28c7cff91887d2a9d20edce61639e0a38
Shalini Mohan <mohan.shalini@outlook.com>
2022-07-02 17:51:23 +0800

    Add shades to default colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
     theme: {
         colors: {
            'blue': '#7087FF',
+           'blue2': '#7087FF75',
+           'blue3': '#7087FF50',
+           'blue4': '#7087FF25',
+
            'green': '#53DFCB',
+           'green2': '#53DFCB75',
+           'green3': '#53DFCB50',
+           'green4': '#53DFCB25',
+
            'yellow': '#FCD47C',
+           'yellow2': '#FCD47C75',
+           'yellow3': '#FCD47C50',
+           'yellow4': '#FCD47C25',
+
            'red': '#F54A87',
-           'white': '#FFFFFF', 
+           'red2': '#F54A8775',
+           'red3': '#F54A8750',
+           'red4': '#F54A8725',
+           
+           'white': '#FFFFFF',
+           'black': '#000000', 
         },
 
         fontFamily: {
