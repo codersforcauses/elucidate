0f419066927a6f0610dcf3443005e08d9bc7985a
Shalini Mohan <mohan.shalini@outlook.com>
2022-07-02 15:00:44 +0800

    Start quiz creation page + add config file

---
+++ client/pages/quiz-creation.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div>
+            <div class="flex place-content-center bg-green">
+                <h1 class="text-center text-3xl font-bold">Create a quiz</h1>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        colors: {
+           'blue': '#7087FF',
+           'green': '#53DFCB',
+           'yellow': '#FCD47C',
+           'red': '#F54A87',
+           'white': '#FFFFFF', 
+        },
+
+        fontFamily: {
+            sans: ['Montserrat', 'sans-serif'],
+        }
+    }
+}
