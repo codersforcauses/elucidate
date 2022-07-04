e45075df7dc5a044954dea3903e9134f646023b7
jsun1590 <jsun1590@gmail.com>
2022-07-04 07:49:15 +0000

    make navbar sticky

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <SectionNavBar />
+  <div class="font-Montserrat">
+    <SectionNavBar class="sticky top-0" />
     <Nuxt />
   </div>
 </template>
+
+<style scoped>
+@import '@fontsource/montserrat';
+@import '@fontsource/open-sans';
+</style>
