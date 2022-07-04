59e10f47518d5236308029ad74a3f810237fbdc3
jordantgiang <jordantgiang@gmail.com>
2022-07-04 11:04:00 +0000

    Create teal/green box component

--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -3,12 +3,12 @@
     <AuthHeader :pageTitle="title" />
     <div class="flex flex-grow justify-center">
       <!-- Add sign up form here? -->
-      <div class="w-11/12 max-w-lg flex flex-col items-center bg-green drop-shadow-lg">
+      <TealBox>
         <form class="flex flex-col w-11/12 mx-6 my-8">
-          <InputField v-for="field in fields" :key="field.index" :fieldName="field.name" :fieldType="field.type"/>
+          <InputField v-for="field in fields" :key="field.index" :fieldName="field.name" :fieldType="field.type" />
         </form>
         <p class="self-end mx-1 mb-1 mt-auto text-white drop-shadow-lg">Continue -></p>
-      </div>
+      </TealBox>
     </div>
     <AuthFooter />
   </div>
