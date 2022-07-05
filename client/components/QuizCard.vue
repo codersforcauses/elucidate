084558cab476c65523bede1f6451c4b31bbe68cb
root <ciaangel11@gmail.com>
2022-07-05 02:04:50 +0000

    Add Quiz Card and Start Button components

---
+++ client/components/QuizCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="p-5 bg-white rounded-xl border border-gray-200 drop-shadow-md">
+    <div class="flex justify-left">
+      <span v-for="tag in tags" :key="tag.tagLabel">
+        <span class="inline-block text-center p-1 mr-2 mb-4
+        bg-yellow-300 rounded-md">{{tag.tagLabel}}
+        </span>
+      </span>
+    </div>
+    <div>
+      <img v-bind:src="arrowImage" class="float-left">
+      <div>
+        <div class="text-black text-3xl ml-10">{{topicName}}</div>
+        <div class="text-black text-xl ml-11 my-1.5">{{subjectName}}</div>
+        <span class="text-black text-md ml-11">{{totalQues}}</span>
+        <span class="text-black text-md ml-5">{{totalHour}}</span><br />
+        <StartButton />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import StartButton from '~/components/StartButton.vue';
+export default {
+  name:'QuizCard',
+  components:{
+    StartButton,
+  },
+  data:function(){
+    return{
+      arrowImage:require("~/assets/Polygon.svg"),
+      topicName:'Topic Name',
+      subjectName:'Subject Name',
+      totalQues:'50 questions',
+      totalHour:'1hr',
+      tags:[
+        {tagLabel: 'Tag 1'},
+        {tagLabel: 'Tag 2'}
+      ]
+    };
+  }
+};
+</script>
--- client/components/haveAccBtn.vue
+++ client/components/haveAccBtn.vue
@@ -1,7 +1,5 @@
 <template>
   <!-- button that directs the user to the log in page -->
-  <!-- button styling: ripple effect, inline-block (prevent the text inside from getting larger than the parent element, px py padding, mb margin, width full, 
-  bg and text color + size,-->
   <a v-bind:href="logInPage"
   data-mdb-ripple="true" data-mdb-ripple-color="light" 
   class="inline-block text-center px-6 py-2.5 mt-6 mb-6 w-full 
