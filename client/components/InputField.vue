3a5697d3bfd8990f72a53e2e09a7b450f3990b15
jordantgiang <jordantgiang@gmail.com>
2022-07-05 16:07:08 +0000

    [WIP] Hide/show password functionality

--- client/components/InputField.vue
+++ client/components/InputField.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <label>{{fieldName}}</label>
-    <input type="fieldType" class="w-full h-10 drop-shadow-lg mt-2 mb-4">
+    <div class="flex items-center">
+      <input v-if="isPassword && !showText" type="password" class="w-full h-10 drop-shadow-lg my-3 px-2" v-model="inputValue">
+      <input v-else type="fieldType" class="w-full h-10 drop-shadow-lg my-3 px-2" v-model="inputValue">
+      <button type="button" v-if="isPassword" v-on:click="clickCheck" class="text-center h-10 px-1 mx-1 bg-white border-2 border-green2 rounded">SHOW</button>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'InputField',
+    data: () => ({
+      showText: false,
+      inputValue: "",
+    }),
     props: {
       fieldName: String,
       fieldType: String,
+      isPassword: Boolean,
+    },
+    methods: {
+      clickCheck: function() {
+        this.showText = !this.showText
+        console.log(this.inputValue)
+      }
     }
   }
 </script>
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -1,18 +1,22 @@
 <template>
-  <body class="flex flex-col min-h-screen">
-    <AuthHeader />
+  <div class="flex flex-col min-h-screen">
+    <AuthHeader :pageTitle="title" />
 
-    <main class="flex flex-grow justify-center">
+    <div class="flex flex-grow justify-center">
       <!-- Add sign up form here? -->
-      <div class="w-11/12 max-w-lg flex flex-col items-center bg-green drop-shadow-lg">
+      <TealBox>
         <form class="flex flex-col w-11/12 mx-6 my-8">
-          <InputField v-for="field in fields" :key="field.index" :fieldName="field.name" :fieldType="field.type"/>
+          <InputField v-for="field in fields" 
+          :key="field.index" 
+          :fieldName="field.name" 
+          :fieldType="field.type" 
+          :isPassword="field.isPassword" />
         </form>
         <p class="self-end mx-1 mb-1 mt-auto text-white drop-shadow-lg">Continue -></p>
-      </div>
+      </TealBox>
     </div>
     <AuthFooter />
-  </body>
+  </div>
 </template>
 
 <script>
@@ -20,13 +24,14 @@ let count = 0;
 export default {
   name: 'signup-page',
   data: () => ({
+    title: "Sign-Up",
     fields: [
-      { name: "First Name", type: "text", index: count++ },
-      { name: "Last Name", type: "text", index: count++ },
-      { name: "Email", type: "text", index: count++ },
-      { name: "Password", type: "text", index: count++ },
-      { name: "Confirm Password", type: "text", index: count++ },
-      { name: "Grade", type: "text", index: count++ },
+      { name: "First Name", type: "text", isPassword: false, index: count++ },
+      { name: "Last Name", type: "text", isPassword: false, index: count++ },
+      { name: "Email", type: "text", isPassword: false, index: count++ },
+      { name: "Password", type: "text", isPassword: true, index: count++ },
+      { name: "Confirm Password", type: "text", isPassword: true, index: count++ },
+      { name: "Grade", type: "text", isPassword: false, index: count++ },
     ]
   })
 };
