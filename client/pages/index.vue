5322ab5e3e5bcf93a7284ca33622edad0e87dce0
root <ciaangel11@gmail.com>
2022-07-05 02:05:48 +0000

    Fix the layout and add section 2 of the home page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="container mx-auto pl-20">
-    <div class="grid grid-cols-2 gap-x-10 items-center">
-      <div>
-        <h1 class="text-black-400 text-6xl leading-snug mb-10">What do you want <br>to learn today?</h1>
-        <SearchBar />
-        <haveAccBtn />
-        <AuthFooter />
+  <div>
+    <div class="container mx-auto pl-20">
+      <!-- Section 1 -->
+      <div class="grid lg:grid-cols-2 gap-x-14 mt-36 mb-52 items-center">
+        <div>
+          <h1 class="text-black-400 text-6xl leading-snug mb-10">What do you want <br>to learn today?</h1>
+          <SearchBar />
+          <haveAccBtn />
+          <AuthFooter />
+        </div>
+        <div>
+          <img v-bind:src="illustration">
+        </div>
       </div>
-      <div>
-        <img v-bind:src="illustration">
+    </div>
+    <!-- Section 2 (sneak-peak of the quizzes) -->
+    <div class="bg-indigo-500 pt-20 pb-40">
+      <h2 class="text-white text-5xl text-center py-20">Look at our quizzes</h2>
+      <div class="grid lg:grid-cols-3 gap-14 mx-36 content-evenly">
+        <div><QuizCard /></div>
+        <div><QuizCard /></div>
+        <div><QuizCard /></div>
       </div>
     </div>
   </div>
@@ -18,11 +30,13 @@
 import haveAccBtn from '~/components/haveAccBtn.vue';
 import SearchBar from '~/components/searchBar.vue';
 import AuthFooter from '~/components/AuthFooter.vue';
+import QuizCard from '~/components/QuizCard.vue';
 export default {
   components: { 
     haveAccBtn,
     SearchBar,
     AuthFooter, 
+    QuizCard,
   },
   name: 'IndexPage',
   data:function() {
