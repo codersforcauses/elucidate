24d69a0165dce51064ea43ca867426c32ea3e3c8
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-05 16:37:41 +0800

    Add drop shadow md to theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-    theme: {
-        colors: {
-           'blue': '#7087FF',
-           'blue2': '#7087FF75',
-           'blue3': '#7087FF50',
-           'blue4': '#7087FF25',
+  theme: {
+    colors: {
+      blue: '#7087FF',
+      blue2: '#7087FF75',
+      blue3: '#7087FF50',
+      blue4: '#7087FF25',
 
-           'green': '#53DFCB',
-           'green2': '#53DFCB75',
-           'green3': '#53DFCB50',
-           'green4': '#53DFCB25',
+      green: '#53DFCB',
+      green2: '#53DFCB75',
+      green3: '#53DFCB50',
+      green4: '#53DFCB25',
 
-           'yellow': '#FCD47C',
-           'yellow2': '#FCD47C75',
-           'yellow3': '#FCD47C50',
-           'yellow4': '#FCD47C25',
+      yellow: '#FCD47C',
+      yellow2: '#FCD47C75',
+      yellow3: '#FCD47C50',
+      yellow4: '#FCD47C25',
 
-           'red': '#F54A87',
-           'red2': '#F54A8775',
-           'red3': '#F54A8750',
-           'red4': '#F54A8725',
-           
-           'white': '#FFFFFF',
-           'black': '#000000', 
-        },
+      red: '#F54A87',
+      red2: '#F54A8775',
+      red3: '#F54A8750',
+      red4: '#F54A8725',
 
-        fontFamily: {
-            sans: ['Montserrat', 'sans-serif'],
-        }
-    }
-}
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+
+    dropShadow: {
+      md: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    },
+
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+  },
+};
