330b71b8b914abca1cca198ee770cdf90cccd730
David <david.holt2002@gmail.com>
2022-07-06 16:15:24 +0800

    Add some more features

---
+++ client/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div :class="`flex ${isCentered ? 'place-content-center' : ''} bg-green`">
+            <h1 class="text-3xl font-bold">{{ title }}</h1>
+        </div>
+    <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    Name: 'Box',
+    props: {
+        title: String,
+        isCentered: Boolean
+    }
+}
+</script>
--- client/pages/quiz-creation.vue
+++ client/pages/quiz-creation.vue
@@ -1,33 +1,25 @@
 <template>
     <div class="flex flex-grow flex-col items-center content-center">
-        <AuthHeader />
             <div>
                 <div class="flex place-content-center bg-green">
                     <h1 class="text-center text-3xl font-bold">Create a quiz</h1>
                 </div>
-                <AuthForm>
+                <Box>
                     <TextBox header="Quiz Name" inputType="text" />
                     <TextBox header="Tags" inputType="text"/>
                     <TextBox header="Time Limit" inputType="number"/>
-                </AuthForm>
+                </Box>
             </div>
-        <AuthFooter />
 
     </div>
 </template>
 
 <script>
-import AuthFooter from '~/components/AuthFooter.vue';
-import AuthHeader from '~/components/AuthHeader.vue';
-import AuthForm from '~/components/form/AuthForm.vue';
-import TextBox from '~/components/form/FormTextBox.vue';
+import Box from '~/components/Box.vue';
 
 export default {
   components: {
-    AuthFooter,
-    AuthHeader,
-    AuthForm,
-    TextBox,
+    Box,
   },
 };
 </script>
