eb3d7b680317ffde4b46030c9bf55f15705b2170
Carmen <carmen020824@gmail.com>
2022-07-07 21:06:25 +0800

    [WIP] add validation feature

--- client/components/InputField.vue
+++ client/components/InputField.vue
@@ -2,29 +2,66 @@
   <div>
     <label>{{fieldName}}</label>
     <div class="flex items-center">
-      <input v-if="isPassword && !showText" type="password" class="w-full h-10 drop-shadow-lg my-3 px-2" v-model="inputValue">
-      <input v-else type="fieldType" class="w-full h-10 drop-shadow-lg my-3 px-2" v-model="inputValue">
+      <ValidationProvider :rules="rules" v-slot="{ errors }" class="w-full">
+        <input v-if="isPassword && !showText" :name ="fieldName" type="password" class="w-full h-10 drop-shadow-lg my-3 px-2" :value="inputvalue" @input="$emit('update:inputvalue', $event.target.value)">
+        <input v-else type="fieldType" :name ="fieldName" class="w-full h-10 drop-shadow-lg my-3 px-2" :value="inputvalue" @input="$emit('update:inputvalue', $event.target.value)">
+        <span class="text-red">{{ errors[0] }}</span>
+      </ValidationProvider>
       <button type="button" v-if="isPassword" v-on:click="clickCheck" class="text-center h-10 px-1 mx-1 bg-white border-2 border-green2 rounded">SHOW</button>
     </div>
   </div>
 </template>
 
+<!-- <script src="./Validator.js"></script> -->
+
 <script>
+// Validation
+// ---------------------------------------
+import { ValidationProvider, extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
+extend('email',{ 
+  ...email,
+  message:'Invalid email address'
+
+});
+
+extend('required',{
+  ...required,
+  message:'This field is required'
+});
+
+extend('password', {
+  params: ['target'],
+  validate(value, { target }) {
+    console.log(target)
+    return value === target;
+  },
+  message: 'Password confirmation does not match'
+});
+
+
+// ---------------------------------------
   export default {
     name: 'InputField',
+    components: {
+    ValidationProvider
+    },
     data: () => ({
       showText: false,
-      inputValue: "",
+      test: "test"
     }),
     props: {
       fieldName: String,
       fieldType: String,
       isPassword: Boolean,
+      rules: String,
+      inputvalue: String
     },
     methods: {
       clickCheck: function() {
         this.showText = !this.showText
-        console.log(this.inputValue)
+        console.log(this.value)
       }
     }
   }
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -6,33 +6,51 @@
       <!-- Add sign up form here? -->
       <TealBox>
         <form class="flex flex-col w-11/12 mx-6 my-8">
-          <InputField v-for="field in fields" 
+          <ValidationObserver>
+          <InputField  v-for="field in fields" 
           :key="field.index" 
           :fieldName="field.name" 
           :fieldType="field.type" 
-          :isPassword="field.isPassword" />
+          :isPassword="field.isPassword"
+          :rules="field.rules"
+          :inputvalue.sync="field.value" />
+          </ValidationObserver>
+          
         </form>
-        <p class="self-end mx-1 mb-1 mt-auto text-white drop-shadow-lg">Continue -></p>
+        <button v-on:click="printthis()">Click</button>
+        <p>data is {{fields[0]["value"]}}</p>
+        <p class="self-end mx-1 mb-1 mt-auto text-white drop-shadow-lg">Continue -></p>    
       </TealBox>
+
     </div>
     <AuthFooter />
   </div>
 </template>
 
 <script>
+import {  ValidationObserver } from 'vee-validate';
+
 let count = 0;
-export default {
+export default{
   name: 'signup-page',
+  components: {
+    ValidationObserver,
+  },
   data: () => ({
     title: "Sign-Up",
     fields: [
-      { name: "First Name", type: "text", isPassword: false, index: count++ },
-      { name: "Last Name", type: "text", isPassword: false, index: count++ },
-      { name: "Email", type: "text", isPassword: false, index: count++ },
-      { name: "Password", type: "text", isPassword: true, index: count++ },
-      { name: "Confirm Password", type: "text", isPassword: true, index: count++ },
-      { name: "Grade", type: "text", isPassword: false, index: count++ },
+      { name: "First Name", type: "text", isPassword: false, index: count++,rules:"required",value:"hi" },
+      { name: "Last Name", type: "text", isPassword: false, index: count++,rules:"required",value:"" },
+      { name: "Email", type: "text", isPassword: false, index: count++,rules:"required|email",value:"" },
+      { name: "Password", type: "text", isPassword: true, index: count++,rules:"required|password:@confirm",value:"" },
+      { name: "confirm", type: "text", isPassword: true, index: count++,rules:"required",value:"" },
+      { name: "Grade", type: "text", isPassword: false, index: count++,rules:"required",value:"" }, 
     ]
-  })
+  }),
+  methods: {
+    printthis:function (){
+      console.log(this.fields[0].value)
+    }
+  },
 };
 </script>
