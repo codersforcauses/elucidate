292ea4d045b6167cb08d4c40dd749330f3fce4e1
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-09 15:44:20 +0800

    Add WIP FormTextBox tests

--- client/components/Form/FormTextBox.vue
+++ client/components/Form/FormTextBox.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="flex flex-col mb-7">
-    <h2 class="font-semibold mb-0.5">{{ header }}</h2>
+    <h2 data-test="formTextBoxLabel" class="font-semibold mb-0.5">
+      {{ header }}
+    </h2>
     <input :type="inputType" class="drop-shadow-md h-11 w-full px-2" />
-    <p v-if="subtext !== null && subtextLink === null">
+    <p
+      v-if="subtext !== null && subtextLink === null"
+      data-test="formTextBoxSubText"
+    >
       {{ subtext }}
     </p>
     <a
       v-else-if="subtextLink !== null"
+      data-test="formTextBoxSubText"
       :href="subtextLink"
       class="place-self-end text-xs text-white font-semibold underline underline-offset-2 pt-1"
     >
