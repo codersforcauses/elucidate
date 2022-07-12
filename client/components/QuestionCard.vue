ea5c5c226f9b84fae9638617bd21302ff5d76a80
RupesAUS <incrediblerupesau@gmail.com>
2022-07-12 22:07:09 +0800

    Add next/prev question functionality

--- client/components/QuestionCard.vue
+++ client/components/QuestionCard.vue
@@ -3,27 +3,30 @@
     <!-- Question Number -->
     <div class="bg-teal-100 py-4 rounded-sm">
         <h1 class="text-center">
-            Question Numb
-            <ProgressBar/>
+            Question  {{this.curr}} / {{this.max}}
+            <ProgressBar 
+            :curr = "this.curr"
+            :max = "this.max"
+            />
         </h1>
     </div>
     <!-- Question Detail -->
     <div class="">
         <div class="justify-self-start text-sm">
             Question Details 
-            ALorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
         </div> 
     </div>
 
     <!-- Next/Back buttons -->
     <div class="flex justify-end px-5 mt-2">
       <button>
-        <div class="inline-block text-center text-xs text-white py-0.5 px-2 mr-1 bg-yellow-600 rounded-xl shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-700 focus:shadow-lg focus:outline-none active:bg-yellow-800 active:shadow-lg transition duration-300 ease-in-out">
+        <div class="inline-block text-center text-xs text-white py-0.5 px-2 mr-1 bg-yellow-600 rounded-xl shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-700 focus:shadow-lg focus:outline-none active:bg-yellow-800 active:shadow-lg transition duration-300 ease-in-out" @click="prevQuestion">
             Back
         </div>
       </button>
       <button>
-        <div class="inline-block text-center text-xs text-white py-0.5 px-2 bg-pink-600 rounded-xl shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none active:bg-pink-800 active:shadow-lg transition duration-300 ease-in-out">
+        <div class="inline-block text-center text-xs text-white py-0.5 px-2 bg-pink-600 rounded-xl shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none active:bg-pink-800 active:shadow-lg transition duration-300 ease-in-out" @click="nextQuestion">
             Next
         </div>
       </button>
@@ -37,8 +40,20 @@ export default {
     name: "QuestionCard",
     data: function () {
         return {
-            quizID: "num"
+            quizID: "num",
+            curr: 1,
+            max: 20
         };
+    },
+    methods: {
+      nextQuestion(){
+        this.curr++
+        // console.log(this.curr);
+      },
+      prevQuestion(){
+        this.curr--
+        // console.log(this.curr);
+      }
     },
     components: { ProgressBar }
 };
