c40174ce9e3f8c24bc9aeb675d2896f79d2b07ac
Carmen <carmen020824@gmail.com>
2022-07-13 16:08:53 +0800

    fix input validation's error message

--- client/components/form/AuthInputField.vue
+++ client/components/form/AuthInputField.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <label>{{ fieldName }}</label>
-    <ValidationProvider :vid="id" :rules="rules" v-slot="{ errors }">
+    <ValidationProvider :vid="id" :rules="rules" v-slot="{ errors, touched }">
       <div class="relative flex items-center">
         <input
           :name="fieldName"
@@ -9,16 +9,20 @@
           class="w-full h-10 px-2 my-3 drop-shadow-lg"
           v-model="inputValue"
         />
-        <div 
+        <div
           v-if="fieldType == 'password'"
           @mouseenter="clickCheck"
           @mouseleave="clickCheck"
           class="w-10 absolute top-auto left-auto bottom-auto right-0"
         >
-          <font-awesome-icon :icon="['fas', 'fa-eye']" class="w-6 mx-2" :class="showText ? 'text-blue2' : 'text-gray-400'" />
+          <font-awesome-icon
+            :icon="['fas', 'fa-eye']"
+            class="w-6 mx-2"
+            :class="showText ? 'text-blue2' : 'text-gray-400'"
+          />
         </div>
       </div>
-      <span class="text-red">{{ errors[0] }}</span>
+      <span v-if="touched" class="text-red">{{ errors[0] }}</span>
     </ValidationProvider>
   </div>
 </template>
@@ -58,11 +62,11 @@ export default {
   components: {
     ValidationProvider,
   },
-  data: function() {
+  data: function () {
     return {
       showText: false,
-      inputValue: "",
-    }
+      inputValue: '',
+    };
   },
   props: {
     fieldName: String,
@@ -73,7 +77,7 @@ export default {
   },
   methods: {
     clickCheck: function () {
-      this.showText = !this.showText
+      this.showText = !this.showText;
     },
   },
 };
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -5,16 +5,30 @@
     <div class="flex justify-center flex-grow">
       <AuthForm>
         <ValidationObserver v-slot="{ invalid }" class="w-full">
-          <form @submit.prevent="onSubmit" class="flex flex-col w-11/12 mx-6 my-8">
-            <InputField v-for="field in fields" 
-            :key="field.index" 
-            :fieldName="field.name" 
-            :fieldType="field.type" 
-            :rules="field.rules"
-            :inputvalue.sync="field.value"
-            :id = "field.id" />
-          <button type="submit" :class="invalid ? 'text-darkgrey bg-lightgrey':'text-red bg-white'" class="w-24 h-8 mt-5 font-bold border border-solid rounded place-self-center text-l drop-shadow-lg">Submit</button>
-          <!-- <button v-else type="submit" :disabled="invalid" class="w-24 h-8 mt-5 font-bold bg-white border border-solid rounded place-self-center text-l text-red">Submit</button> -->
+          <form
+            @submit.prevent="onSubmit"
+            class="flex flex-col w-11/12 mx-6 my-8"
+          >
+            <InputField
+              v-for="field in fields"
+              :key="field.index"
+              :fieldName="field.name"
+              :fieldType="field.type"
+              :rules="field.rules"
+              :inputvalue.sync="field.value"
+              :id="field.id"
+            />
+            <button
+              type="submit"
+              :disabled="invalid"
+              :class="
+                invalid ? 'text-darkgrey bg-lightgrey' : 'text-red bg-white'
+              "
+              class="w-24 h-8 mt-5 font-bold border border-solid rounded place-self-center text-l drop-shadow-lg"
+            >
+              Submit
+            </button>
+            <!-- <button v-else type="submit" :disabled="invalid" class="w-24 h-8 mt-5 font-bold bg-white border border-solid rounded place-self-center text-l text-red">Submit</button> -->
           </form>
         </ValidationObserver>
       </AuthForm>
@@ -26,14 +40,14 @@
 </template>
 
 <script>
-import {ValidationObserver} from 'vee-validate';
+import { ValidationObserver } from 'vee-validate';
 import AuthFooter from '~/components/AuthFooter.vue';
 import AuthHeader from '~/components/AuthHeader.vue';
 import AuthForm from '~/components/form/AuthForm.vue';
 import InputField from '~/components/form/AuthInputField.vue';
 
 let count = 0;
-export default{
+export default {
   name: 'signup-page',
   layout: 'auth',
   components: {
@@ -44,24 +58,60 @@ export default{
     InputField,
   },
   data: () => ({
-    title: "Sign-Up",
-    password:'',
-    confirm : '',
+    title: 'Sign-Up',
+    password: '',
+    confirm: '',
     ispassword: true,
     btnDisable: true,
     fields: [
-      { name: "First Name", type: "text", index: count++, id:"firstname", rules:"required" },
-      { name: "Last Name", type: "text", index: count++, id:"lastname", rules:"required" },
-      { name: "Email", type: "text", index: count++, id:"email", rules:"required|email"},
-      { name: "Password", type: "password", index: count++, id:"password", rules:"required|min:6"},
-      { name: "Confirm Password", type: "password", index: count++, id:"confirm", rules:"required|password:@password" },
-      { name: "Grade", type: "text", index: count++, id:"grade", rules:"required" }, 
-    ]
+      {
+        name: 'First Name',
+        type: 'text',
+        index: count++,
+        id: 'firstname',
+        rules: 'required',
+      },
+      {
+        name: 'Last Name',
+        type: 'text',
+        index: count++,
+        id: 'lastname',
+        rules: 'required',
+      },
+      {
+        name: 'Email',
+        type: 'text',
+        index: count++,
+        id: 'email',
+        rules: 'required|email',
+      },
+      {
+        name: 'Password',
+        type: 'password',
+        index: count++,
+        id: 'password',
+        rules: 'required|min:6',
+      },
+      {
+        name: 'Confirm Password',
+        type: 'password',
+        index: count++,
+        id: 'confirm',
+        rules: 'required|password:@password',
+      },
+      {
+        name: 'Grade',
+        type: 'text',
+        index: count++,
+        id: 'grade',
+        rules: 'required',
+      },
+    ],
   }),
   methods: {
-    onSubmit () {
+    onSubmit() {
       alert('Form has been submitted!');
-    }
-  }
+    },
+  },
 };
 </script>
