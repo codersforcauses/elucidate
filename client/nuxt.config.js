74ec734ec67dd6d1f5ee0c6ed2d870ed0a5f35ca
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-13 21:30:35 +0800

    Merge configs from i76

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,7 +20,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@fontsource/montserrat/variable.css'],
+
+  css: [
+    '@fontsource/montserrat/variable.css',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/fontawesome.js'],
