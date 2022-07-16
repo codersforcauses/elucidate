67aae3c93134fb87e8bd088baac7a41538c70601
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-16 16:00:46 +0800

    temp divs

--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
-  
+    <div class="bg-red flex items-center justify-between h-full shadow-xl">
+      <div> 
+      </div>
+      <div> 
+      </div>
+    </div>
   </main>
 </template>
 
