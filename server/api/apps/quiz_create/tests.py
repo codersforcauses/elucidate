0e2fd5403470db4dc9a3db34d1f5aa7df15535c7
LJW-Dev <lukewaters757@gmail.com>
2022-07-16 16:05:08 +0800

    feat(quiz): Added DELETE and PUT api methods and added tests

--- server/api/apps/quiz_create/tests.py
+++ server/api/apps/quiz_create/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .views import *
+from .serializers import *
+import json
 
-# Create your tests here.
+def ByteToJSON(JSONBytes):
+    return json.loads(JSONBytes.decode('utf-8'))
+
+
+class CreateQuizTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.post('/quiz-api/question/', { 'question_type': 0, 'question': 'Test Question 1'})
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['question_type'], 0)
+        self.assertEqual(json['question'], 'Test Question 1')
+        question_id = json['id']
+
+        response = self.client.get('/quiz-api/question/')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json[0]['question'], 'Test Question 1')
+
+        response = self.client.get(F'/quiz-api/question/{question_id}/')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['question_type'], 0)
+        self.assertEqual(json['question'], 'Test Question 1')
+
+        response = self.client.put(F'/quiz-api/question/{question_id}/', { 'question_type': 2}, content_type='application/json')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['question_type'], 2)
+
+        response = self.client.post(F'/quiz-api/question/{question_id}/answer/', { 'is_correct': True, 'answer': 'Test Answer 1'})
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['is_correct'], True)
+        self.assertEqual(json['answer'], 'Test Answer 1')
+
+        response = self.client.get(F'/quiz-api/question/{question_id}/answer/')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json[0]['is_correct'], True)
+        self.assertEqual(json[0]['answer'], 'Test Answer 1')
+        answer_id = json[0]['id']
+
+        response = self.client.get(F'/quiz-api/question/{question_id}/answer/{answer_id}')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['is_correct'], True)
+        self.assertEqual(json['answer'], 'Test Answer 1')
+        
+        response = self.client.put(F'/quiz-api/question/{question_id}/answer/{answer_id}', { 'is_correct': False}, content_type='application/json')
+        json = ByteToJSON(response.content)
+        self.assertEqual(json['is_correct'], False)
+
+
+
+
+
+        # self.assertEqual(request.data, 201)
+    
+
--- server/api/apps/quiz_create/views.py
+++ server/api/apps/quiz_create/views.py
@@ -1,37 +1,55 @@
 from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework.status import *
+from rest_framework.decorators import api_view
 from .serializers import *
 
 from django.views.decorators.csrf import csrf_exempt  # will change this later
 
 
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def QuestionTask(request):
     if(request.method == 'GET'):    
         question = Quiz_Question.objects.all()
         serializer = Quiz_QuestionSerializer(question, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
     elif(request.method == 'POST'):
-        data = JSONParser().parse(request)
-        serializer = Quiz_QuestionSerializer(data=data)
+        serializer = Quiz_QuestionSerializer(data=request.data)
         if(serializer.is_valid(raise_exception=True)):
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    
     else:
-        return HttpResponse(status=400)  # bad request
+        return HttpResponse(status=HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def SpecificQuestionTask(request, question_pk):
     question = get_object_or_404(Quiz_Question, pk=question_pk)
     
     if(request.method == 'GET'):
         serializer = Quiz_QuestionSerializer(question)
         return JsonResponse(serializer.data)
+
+    elif(request.method == 'PUT'):
+        serializer = Quiz_QuestionSerializer(question, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(serializer.data)
+    
+    elif(request.method == 'DELETE'):
+        question.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
     else:
-        return HttpResponse(status=400)  # bad request
+        return HttpResponse(status=HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def AnswerTask(request, question_pk):
     question = get_object_or_404(Quiz_Question, pk=question_pk)
     
@@ -39,27 +57,43 @@ def AnswerTask(request, question_pk):
         answers = question.question_answer_set.all()
         serializer = Question_AnswerSerializer(answers, many=True)
         return JsonResponse(serializer.data, safe=False)
+
     elif(request.method == 'POST'):
-        data = JSONParser().parse(request)
-        serializer = Question_AnswerSerializer(data=data)
+        serializer = Question_AnswerSerializer(data=request.data)
         if(serializer.is_valid(raise_exception=True)):
             question.question_answer_set.create(is_correct=serializer.data['is_correct'], answer=serializer.data['answer'])
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=HTTP_201_CREATED)
+    
     else:
-        return HttpResponse(status=400)  # bad request
+        return HttpResponse(status=HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def SpecificAnswerTask(request, question_pk, answer_pk):
     question = get_object_or_404(Quiz_Question, pk=question_pk)
+    answer = get_object_or_404(question.question_answer_set, pk=answer_pk)
     
     if(request.method == 'GET'):
-        answer = get_object_or_404(Question_Answer, pk=answer_pk)
+        # answer = get_object_or_404(Question_Answer, pk=answer_pk)
         serializer = Question_AnswerSerializer(answer)
         return JsonResponse(serializer.data)
+
+    elif(request.method == 'PUT'):
+        # answer = get_object_or_404(Question_Answer, pk=answer_pk)
+        serializer = Question_AnswerSerializer(answer, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(serializer.data)
+    
+    elif(request.method == 'DELETE'):
+        answer.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+    
     else:
-        return HttpResponse(status=400)  # bad request
+        return HttpResponse(status=HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def TagTask(request, question_pk):
     question = get_object_or_404(Quiz_Question, pk=question_pk)
 
@@ -67,13 +101,15 @@ def TagTask(request, question_pk):
         tags = question.question_tag_set.all()
         serializer = Question_TagSerializer(tags, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
     elif(request.method == 'POST'):
         data = JSONParser().parse(request)
         serializer = Question_TagSerializer(data=data)
         if(serializer.is_valid(raise_exception=True)):
             question.question_tag_set.create(tag=serializer.data['tag'])
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=HTTP_201_CREATED)
+    
     else:
-        return HttpResponse(status=400)  # bad request
+        return HttpResponse(status=HTTP_400_BAD_REQUEST)
 
 
