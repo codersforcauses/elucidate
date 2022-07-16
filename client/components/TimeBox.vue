9c8973402529707af8f9c28620def407ce6e8a05
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-16 15:02:05 +0800

    Fix stopwatch icon

--- client/components/TimeBox.vue
+++ client/components/TimeBox.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="mb-5 ml-20">
-        <label class="p-10 text-white text-xl font-bold"><font-awesome-icons icon="fa-regular fa-stopwatch"/>{{ header }}</label>
-        <input class="p-0 m-0 w-1/2 h-9 rounded shadow-sm" type="time">
-    </div>
+  <div class="mb-5 ml-20">
+    <label class="p-10 text-white text-xl font-bold">
+      <FontAwesomeIcon :icon="['fa', 'stopwatch']" />
+      {{ header }}
+    </label>
+    <input class="p-0 m-0 w-1/2 h-9 rounded shadow-sm" type="time" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'TimeBox',
-    props: {
-        header: String,
-
-    }
-}
+  name: 'TimeBox',
+  props: {
+    header: String,
+  },
+};
 </script>
