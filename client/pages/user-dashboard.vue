dc3319244b010c814d38fc2b9e0ccf91489d983a
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-17 23:17:59 +0800

    nuxtimg for profile pic

--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -1,23 +1,29 @@
 <template>
   <main>
+    w
     <div class="flex items-center h-screen">
-      <div class="p-9 bg-green4 flex-none w-1/3 h-full">
-
+      <div class="p-9 flex-none w-1/3 h-full bg-blue4">
         <!-- background image, along with alignment of elements -->
         <div
-          class="bg-[url('~/static/temp_Shapebg.svg')] justify-center content-center bg-contain bg-no-repeat bg-center h-full w-full"
+          class="bg-[url('~/static/temp_Shapebg.svg')] bg-green4 justify-center bg-contain bg-no-repeat bg-center h-full w-full"
         >
           <!-- to be Profile pic -->
-          <div class=""></div>
+          <nuxt-img
+            src="branding/logo_full.png"
+            class="h-12"
+            alt="Elucidate Logo Full"
+          />
 
           <!-- to be Welcome text -->
-          <div class="text-center font-bold text-xl">Welcome {{ Temp_testPerson.name }}</div>
+          <div class="text-center content-around font-bold text-xl">
+            Welcome {{ Temp_testPerson.name }}
+          </div>
 
           <!-- to be your Email -->
           <div class="text-center font-thin">{{ Temp_testPerson.email }}</div>
 
           <!-- to be Edit Profile Button-->
-          <div class="text-center">edit profile</div>
+          <div class="text-center">edit profile button</div>
         </div>
       </div>
 
