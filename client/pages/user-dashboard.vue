6ca42eb7ef63419f79163375e6312ab94914c691
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-17 22:36:02 +0800

    adding divs to be features

--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -2,14 +2,22 @@
   <main>
     <div class="flex items-center justify-between h-screen"> 
       <div class="pg-11  p-11 my-4 bg-green4 flex-none w-1/3 h-full"> 
-        <div class="bg-[url('~/static/Shapebg.svg')] bg-contain bg-no-repeat bg-center h-full w-full">
+        <div class="bg-[url('~/static/temp_Shapebg.svg')] bg-contain bg-no-repeat bg-center h-full w-full">
+
+          <!-- to be Profile pic -->
           <div class=""> 
 
           </div>
 
+          <!-- to be Welcome text -->
           <div class="">
           </div>
 
+          <!-- to be your Email -->
+          <div class="">
+          </div>
+          
+          <!-- to be Edit Profile Button-->
           <div class="">
           </div>
         </div>
