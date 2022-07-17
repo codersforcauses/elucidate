40e381f6aa83b4fa00fe5162543cf1b5fbd24920
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-17 19:09:31 +0800

    fixing divs to work with new default page

--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
-    <div class="bg-red flex items-center justify-between h-full shadow-xl">
-      <div class="bg-green flex-none w-1/4 h-full">
+    <div class="bg-red flex items-center justify-between h-screen shadow-xl">
+      <div class="bg-green flex-none w-1/4 h-full"> asdf
       </div>
-      <div class="bg-yellow flex-1 flex-row h-full">
+      <div class="bg-yellow flex-1 flex-row h-full"> asdf
       </div>
     </div>
   </main>
