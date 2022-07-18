4f2aab139e9583fc19c3c1e61fe340e9589f19f4
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-18 22:22:56 +0800

    switching to leftbanner component

--- client/components/Dashboard/LeftBanner.vue
+++ client/components/Dashboard/LeftBanner.vue
@@ -1,5 +1,27 @@
 <template>
+  <!-- background image, along with alignment of elements -->
+  <div
+    class="flex flex-col justify-center items-center content-evenly space-y-3 h-full bg-[url('~/static/temp_Shapebg.svg')] bg-contain bg-no-repeat bg-center"
+  >
+    <!-- to be Profile pic -->
+    <nuxt-img
+      :src="User.profPic ? User.profPic : '/avatar.svg'"
+      class="h-52"
+      :alt="User.name + ' profile picture'"
+    />
 
+    <!-- to be Welcome text -->
+    <div>
+      <p class="font-bold text-xl">Welcome {{ User.name }}</p>
+    </div>
+
+    <!-- to be your Email -->
+    <div class="font-thin">{{ User.email }}</div>
+
+    <ButtonElement to="/edit-Profile" type="primary" class=""
+      >Edit Profile</ButtonElement
+    >
+  </div>
 </template>
 
 <script>
--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -1,50 +1,20 @@
 <template>
   <main>
     <div class="flex h-screen">
-
       <!-- left hand side -- should be component ? -->
       <div class="p-9 flex-none w-1/3 h-full">
-        <!-- background image, along with alignment of elements -->
-        <div
-          class="flex flex-col justify-center items-center content-evenly space-y-3 h-full bg-[url('~/static/temp_Shapebg.svg')] bg-contain bg-no-repeat bg-center"
-        >
-          <!-- to be Profile pic -->
-          <nuxt-img
-            :src="User.profPic ? User.profPic : '/avatar.svg'"
-            class="h-52"
-            :alt="User.name + ' profile picture'"
-          />
-
-          <!-- to be Welcome text -->
-          <div>
-            <p class="font-bold text-xl">
-              Welcome {{ User.name }}
-            </p>
-          </div>
-
-          <!-- to be your Email -->
-          <div class="font-thin">{{ User.email }}</div>
-
-          <ButtonElement to="/edit-Profile" type="primary" class=""
-            >Edit Profile</ButtonElement
-          >
-        </div>
+        <LeftBanner />
       </div>
 
       <!-- right hand side - should be component?-->
       <div class="flex-1 flex-row h-full">
         <!-- right hand side, top  -->
         <div class="">
-          <h1 class="font-bold text-xl" > 
-            Overview
-          </h1>
+          <h1 class="font-bold text-xl">Overview</h1>
         </div>
         <!-- right hand side, bottom part -->
         <div class="">
-          <h1 class="font-bold text-xl"> 
-            Your Quizzes
-          </h1>
-
+          <h1 class="font-bold text-xl">Your Quizzes</h1>
         </div>
       </div>
     </div>
@@ -52,7 +22,9 @@
 </template>
 
 <script>
+import LeftBanner from '../components/Dashboard/LeftBanner.vue';
 export default {
+  components: { LeftBanner },
   name: 'UserDashboard',
 
   data: () => ({
@@ -63,15 +35,14 @@ export default {
       email: 'LoremIpsum@gmail.com',
       profPic: 'tempProfPic.png',
     },
-
   }),
   beforeMount() {
     this.User = this.getUserData();
-  }, 
+  },
   methods: {
     getUserData() {
       return this.Temp_testPerson;
-    }
-  },   
+    },
+  },
 };
 </script>
