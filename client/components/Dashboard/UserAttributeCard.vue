e9b4afa35863a08d8cff59c46ceaeed6c7148eb3
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-18 23:19:58 +0800

    skeleton of Overview

--- client/components/Dashboard/UserAttributeCard.vue
+++ client/components/Dashboard/UserAttributeCard.vue
@@ -1,9 +1,25 @@
 <template>
-
+  <div class="h-72 w-72">
+    <div class="bg-lightgrey/75 h-full">
+      {{property}}
+      {{propertyValue}}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'UserAttributeCard',
+  props: {
+    property: {
+      type: String,
+      default: undefined,
+    },
+    propertyValue: {
+      type: String,
+      default: undefined,
+    }
+
+  }
 };
 </script>
--- client/pages/user-dashboard.vue
+++ client/pages/user-dashboard.vue
@@ -3,7 +3,7 @@
     <div class="flex h-screen">
       <!-- left hand side -- should be component ? -->
       <div class="p-9 flex-none w-1/3 h-full">
-        <LeftBanner 
+        <LeftBanner
           :email="Temp_testPerson.email"
           :name="User.name"
           :profilePic="User.profilePic"
@@ -13,8 +13,13 @@
       <!-- right hand side - should be component?-->
       <div class="flex-1 flex-row h-full">
         <!-- right hand side, top  -->
-        <div class="">
+        <div class="space-y-6">
           <h1 class="font-bold text-xl">Overview</h1>
+          <div class="flex flex-row space-x-6">
+            <UserAttributeCard property="Quizzes Answered"/>
+            <UserAttributeCard property="Quizzes Created"/>
+            <UserAttributeCard property="User Attribute"/>
+          </div>
         </div>
         <!-- right hand side, bottom part -->
         <div class="">
@@ -27,9 +32,11 @@
 
 <script>
 import LeftBanner from '../components/Dashboard/LeftBanner.vue';
+import UserAttributeCard from '../components/Dashboard/UserAttributeCard.vue';
+
 export default {
   name: 'UserDashboard',
-  components: { LeftBanner },
+  components: { LeftBanner, UserAttributeCard },
   data: () => ({
     User: {},
 
