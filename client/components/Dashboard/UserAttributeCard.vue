0210ddf037ea0ebef6912e24b02d56ce1f48bf42
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-19 14:15:44 +0800

    fixing up attribute card styles

--- client/components/Dashboard/UserAttributeCard.vue
+++ client/components/Dashboard/UserAttributeCard.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="h-64 w-64">
-    <div class="flex flex-col space-y-3  justify-center items-center bg-lightgrey/75 h-full">
-      <div v-if="color === 'green'" class="bg-green text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
+  <div class="h-64 w-64 hover:shadow-lg transition-shadow duration-200 ease-in-out">
+    <div class="flex flex-col space-y-3 justify-center items-center bg-lightgrey/75 h-full">
+      <div v-if="color === 'green'" class="bg-green rounded-md text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
         {{propertyValue}}
       </div>
-      <div v-if="color === 'yellow'" class="bg-yellow text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
+      <div v-if="color === 'yellow'" class="bg-yellow rounded-md text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
         {{propertyValue}}
       </div>
-      <div v-if="color === 'red'" class="bg-red text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
+      <div v-if="color === 'red'" class="bg-red rounded-md text-white text-4xl font-extrabold h-20 w-20 flex justify-center items-center">
         {{propertyValue}}
       </div>
       <div>
@@ -15,7 +15,7 @@
           {{property}}
         </p>
       </div>
-      <ButtonElement type="primary"> 
+      <ButtonElement type="tertiary"> 
         View Quizzes
       </ButtonElement>
     </div>
