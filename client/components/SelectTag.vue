998abc94c9a0c6dc48ec06aff41e0d4f269f056c
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-19 16:57:22 +0800

    Merging with i46-quiz_search_page

---
+++ client/components/SelectTag.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative m-1.5">
+    <button @click="$emit('toggleTag', label)" :class=css>{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    css : function () {
+      return this.colour + " rounded";
+    }
+  },
+  props: ['label', 'colour']
+}
+</script>
+
+<style>
+</style>
