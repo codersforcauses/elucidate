afd8e4fdb3f374be16432e09d777752357f20ff8
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-20 15:48:44 +0800

    Adding Quiz Score

--- client/components/Quiz/QuizListItem.vue
+++ client/components/Quiz/QuizListItem.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="w-full border-lightgrey/75 border rounded-md py-2 px-6 mr-5 grid-cols-4 grid">
-    <div>{{Quiz.name}}</div>
-    <div class="flex w-full">
+  <div class="w-full border-lightgrey/75 border rounded-md py-2 px-6 mr-5 grid-cols-9 grid">
+    <div class="col-span-2">{{Quiz.name}}</div>
+    <div class="flex w-full col-span-2">
       <QuizTag v-for="(tag, index) in Quiz.tags" :key="index" :label="tag.name" :colour="tag.color" />
     </div>
-    <div>{{Quiz.author}}</div>
-    <div>{{Quiz.dateCreated}}</div>
+    <div class="col-span-2">{{Quiz.author}}</div>
+    <div class="col-span-2">{{Date(Quiz.dateCreated).slice(0, 21)}}</div>
+    <div>{{Quiz.score ? Quiz.score + "%" : `Not Taken`}} </div>
        
   </div>
 </template>
