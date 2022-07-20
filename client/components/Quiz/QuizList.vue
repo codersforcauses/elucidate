aa15918020db9310baedfaac406e854f3c2fab67
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-20 15:48:27 +0800

    Remaking list size

--- client/components/Quiz/QuizList.vue
+++ client/components/Quiz/QuizList.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="h-full w-full"> 
-    <div class="grid grid-rows-auto mr-10">
-      <div class="py-2 px-6 grid grid-cols-4 text-lightgrey/75 font-thin"> 
-        <h1>Name</h1>
-        <h1>Tags</h1>
-        <h1>Author</h1>
-        <h1>Date Created</h1>
+    <div class="grid grid-rows-auto mr-10 flex space-y-2">
+      <div class="py-1 px-6 grid grid-cols-9 text-lightgrey font-bold flex"> 
+        <h1 class="col-span-2">Name</h1>
+        <h1 class="col-span-2">Tags</h1>
+        <h1 class="col-span-2">Author</h1>
+        <h1 class="col-span-2">Date Created</h1>
+        <h1>Your Score</h1>
       </div>
       <QuizListItem v-for="(quiz, index) in quizzes" :key="index" :Quiz="quiz"/>
       
