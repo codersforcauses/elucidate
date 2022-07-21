52f27b1bb3e93a4c554c1c19575ac7602b18f39d
Jack Sun <jsun1590@gmail.com>
2022-07-21 08:26:33 +0000

    add test for duplicate accounts

--- client/cypress/e2e/auth/signup.spec.cy.js
+++ client/cypress/e2e/auth/signup.spec.cy.js
@@ -11,5 +11,18 @@ describe('Signup', () => {
     cy.get('select').select('Grade 12');
     cy.get('button').should('not.have.attr', 'disabled');
     cy.get('button').click();
+    cy.contains('Congratulations John!');
+  });
+
+  it('diplays an error message when trying to create another account with the same email', () => {
+    cy.get('input[name="First Name"').type('John');
+    cy.get('input[name="Last Name"').type('Doe');
+    cy.get('input[name="Email"').type('john.doe@example.com');
+    cy.get('input[name="Password"').type('password');
+    cy.get('input[name="Confirm Password"').type('password');
+    cy.get('select').select('Grade 12');
+    cy.get('button').should('not.have.attr', 'disabled');
+    cy.get('button').click();
+    cy.contains('user with this email address already exists.');
   });
 });
