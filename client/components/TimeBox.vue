470f85e723d1c3b75b667b9532fa0d70f195e10d
David <david.holt2002@gmail.com>
2022-07-22 14:52:15 +0800

    Changed time box's input box to a better version

--- client/components/TimeBox.vue
+++ client/components/TimeBox.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="mb-5 ml-20">
-    <label class="p-10 text-white text-xl font-bold">
+  <div class="flex flex-inline mb-5 ml-20">
+    <label class="p-0 ml-1 mr-12 text-white text-xl font-bold">
       <FontAwesomeIcon :icon="['fa', 'stopwatch']" />
       {{ header }}
     </label>
-    <input class="p-0 m-0 w-1/2 h-9 rounded shadow-sm" type="time" />
+    <input class="p-2 m-0 w-1/6 h-9 rounded shadow-sm" type="number" />
+    <p class="ml-4 mr-4 text-white text-xl font-bold">mins</p>
+    <input class="p-2 m-0 w-1/6  h-9 rounded shadow-sm" type="number" />
+    <p class="ml-4 mr-4 text-white text-xl font-bold">secs</p>
   </div>
 </template>
 
