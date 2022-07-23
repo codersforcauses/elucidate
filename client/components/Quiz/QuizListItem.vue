bdf872b88894b2ed9e52e8a2254eaebe44fb1543
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-07-23 14:43:32 +0800

    quizlistitem no longer clickable

--- client/components/Quiz/QuizListItem.vue
+++ client/components/Quiz/QuizListItem.vue
@@ -1,19 +1,19 @@
 <template>
   <div
-    @click="$emit('showQuiz', Quiz)"
-    class="w-full border-lightgrey/75 border rounded-md py-2 px-6 mr-5 grid-cols-7 grid hover:border-darkgrey hover:shadow-md transition-all duration-200 ease-in-out"
-  >
-    <div class="col-span-2">{{ Quiz.name }}</div>
-    <div class="flex w-full col-span-2">
+    class="w-full border-lightgrey/75 border rounded-md py-2 px-6 mr-5 grid-cols-7 grid"
+    @click="$emit('showQuiz', quiz)"
+    >
+    <div class="col-span-2">{{ quiz.name }}</div>
+    <div class="flex w-full space-x-2 col-span-2">
       <QuizTag
-        v-for="(tag, index) in Quiz.tags"
+        v-for="(tag, index) in quiz.tags"
         :key="index"
         :label="tag.name"
         :colour="tag.color"
       />
     </div>
-    <div class="col-span-2">{{ Date(Quiz.dateCreated).slice(0, 21) }}</div>
-    <div>{{ Quiz.score ? Quiz.score + '%' : `Not Taken` }}</div>
+    <div class="col-span-2">{{ Date(quiz.dateCreated).slice(0, 21) }}</div>
+    <div>{{ quiz.score ? quiz.score + '%' : `Not Taken` }}</div>
   </div>
 </template>
 
@@ -21,7 +21,7 @@
 export default {
   name: 'QuizListItem',
   props: {
-    Quiz: {
+    quiz: {
       name: '',
       tags: [],
       dateCreated: '',
