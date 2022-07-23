0d82b2722a4efe84b9b313684dc355e904dc7ad5
Jack Sun <jsun1590@gmail.com>
2022-07-23 14:29:48 +0800

    fix cypress test ending bracket

--- client/cypress/e2e/auth/signup.spec.cy.js
+++ client/cypress/e2e/auth/signup.spec.cy.js
@@ -3,11 +3,11 @@ describe('Signup', () => {
     cy.visit('signup');
   });
   it('can signup', () => {
-    cy.get('input[name="First Name"').type('John');
-    cy.get('input[name="Last Name"').type('Doe');
-    cy.get('input[name="Email"').type('john.doe@example.com');
-    cy.get('input[name="Password"').type('password');
-    cy.get('input[name="Confirm Password"').type('password');
+    cy.get('input[name="First Name"]').type('John');
+    cy.get('input[name="Last Name"]').type('Doe');
+    cy.get('input[name="Email"]').type('john.doe@example.com');
+    cy.get('input[name="Password"]').type('password');
+    cy.get('input[name="Confirm Password"]').type('password');
     cy.get('select').select('Grade 12');
     cy.get('button').should('not.have.attr', 'disabled');
     cy.get('button').click();
@@ -15,11 +15,11 @@ describe('Signup', () => {
   });
 
   it('diplays an error message when trying to create another account with the same email', () => {
-    cy.get('input[name="First Name"').type('John');
-    cy.get('input[name="Last Name"').type('Doe');
-    cy.get('input[name="Email"').type('john.doe@example.com');
-    cy.get('input[name="Password"').type('password');
-    cy.get('input[name="Confirm Password"').type('password');
+    cy.get('input[name="First Name"]').type('John');
+    cy.get('input[name="Last Name"]').type('Doe');
+    cy.get('input[name="Email"]').type('john.doe@example.com');
+    cy.get('input[name="Password"]').type('password');
+    cy.get('input[name="Confirm Password"]').type('password');
     cy.get('select').select('Grade 12');
     cy.get('button').should('not.have.attr', 'disabled');
     cy.get('button').click();
