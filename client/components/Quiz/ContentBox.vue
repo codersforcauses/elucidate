3a9cde42e436b72baaebf721b548e38b549708fc
jordantgiang <jordantgiang@gmail.com>
2022-07-23 07:00:45 +0000

    Add content box for the form

---
+++ client/components/Quiz/ContentBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col w-2/3 mx-auto">
+    <div class="bg-green2">
+      <h2 class="m-5 font-bold">{{ title }}</h2>
+    </div>
+    <div class="flex flex-col items-center bg-blue2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentBox',
+  props: {
+    title: String,
+  }
+};
+</script>
--- client/pages/beginquiz.vue
+++ client/pages/beginquiz.vue
@@ -1,10 +1,28 @@
 <template>
-  <h3 class="relative">Hello</h3>
+  <ContentBox class="flex justify-center" title="Begin Quiz">
+    <div class="flex flex-row h-64">
+      <div>
+        <h2 class="text-white my-5">Subject:</h2>
+        <div class="bg-white w-50 h-5 my-5"></div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="flex justify-between mt-auto">
+      <div class="w-10">Button</div>
+    </div>
+  </ContentBox>
 </template>
 
 <script>
+import ContentBox from '~/components/Quiz/ContentBox.vue';
+
 export default {
   name: 'beginquiz',
   layout: 'quizbg',
+  components: {
+    ContentBox
+  }
 };
 </script>
