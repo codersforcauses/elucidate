76d803da7bed8aa2140cb75cf3a1a136511ffccc
cjnbennett <conor.jn.bennett@gmail.com>
2022-07-23 11:28:20 +0800

    fix(quiz): changed urls pattern

--- server/api/urls.py
+++ server/api/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("take-quiz/", include("api.apps.quiz_take.urls")),
     path("admin/", admin.site.urls),
     path("api/auth/", include("api.apps.auth.urls")),
     path("api/user/", include("api.apps.users.urls")),
+    path("api/take-quiz/", include("api.apps.quiz_take.urls")),
 ]
