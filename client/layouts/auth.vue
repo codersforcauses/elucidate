dfaff42719e2ede0ab34c6ed1bdd75332e635683
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-23 11:59:13 +0800

    Set title in auth to route name

--- client/layouts/auth.vue
+++ client/layouts/auth.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="flex flex-col items-center content-center min-w-full min-h-screen">
-    <AuthHeader page-title="Auth Page" />
+  <div
+    class="flex flex-col items-center content-center min-w-full min-h-screen"
+  >
+    <AuthHeader :page-title="$route.name" />
     <Nuxt />
     <AuthFooter />
   </div>
 </template>
-
 <style scoped>
 @import '@fontsource/montserrat';
 @import '@fontsource/open-sans';
