57104ddd28d2ef9557e33e2865e86ad5aa59a10e
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-23 14:11:58 +0800

    Make first letter uppercase in auth title

--- client/layouts/auth.vue
+++ client/layouts/auth.vue
@@ -2,11 +2,24 @@
   <div
     class="flex flex-col items-center content-center min-w-full min-h-screen"
   >
-    <AuthHeader :page-title="$route.name" />
+    <AuthHeader :page-title="title" />
     <Nuxt />
     <AuthFooter />
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      // Make first letter uppercase
+      const rawTitle = this.$route.name;
+      return rawTitle.charAt(0).toUpperCase() + rawTitle.slice(1);
+    },
+  },
+};
+</script>
+
 <style scoped>
 @import '@fontsource/montserrat';
 @import '@fontsource/open-sans';
