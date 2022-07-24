b0180464248a8a4e2af1e1f39982f7d8810a9d3f
Carmen <carmen020824@gmail.com>
2022-07-24 12:30:41 +0800

    add select feature

--- client/pages/beginquiz.vue
+++ client/pages/beginquiz.vue
@@ -3,31 +3,63 @@
     <div class="flex flex-col my-10 w-full h-auto">
       <div class="flex flex-row justify-between items-center w-full h-15 my-5">
         <h2 class="text-white w-1/4 mx-5">Subject:</h2>
-        <div class="bg-white w-2/3 h-auto mx-5 p-2 rounded-lg shadow-lg">Maths</div>
+        <div class="w-2/3 mx-5 rounded-lg shadow-lg">
+          <multiselect
+            v-model="subjectValue"
+            :options="subjectOptions"
+            placeholder="Search or select a subject"
+          ></multiselect>
+        </div>
       </div>
       <div class="flex flex-row justify-between items-center w-full h-15 my-5">
         <h2 class="text-white w-1/4 mx-5">Topic:</h2>
-        <div class="bg-white w-2/3 h-auto mx-5 p-2 rounded-lg shadow-lg">Calculus</div>
+        <!-- <div class="bg-white w-2/3 h-auto mx-5 p-2 rounded-lg shadow-lg"> -->
+        <div class="w-2/3 mx-5 rounded-lg shadow-lg">
+          <multiselect
+            v-model="topicValue"
+            :options="topicOptions"
+            :multiple="true"
+            :close-on-select="false"
+            :clear-on-select="false"
+            :preserve-search="true"
+            :hide-selected="true"
+            placeholder="Search or select topics"
+          >
+          </multiselect>
+        </div>
       </div>
       <div class="flex flex-row justify-between items-center w-full h-15 my-5">
         <h2 class="text-white w-1/4 mx-5">Number of Questions:</h2>
-        <div class="bg-white w-2/3 h-auto mx-5 p-2 rounded-lg shadow-lg">3</div>
+        <div class="bg-white w-2/3 h-auto mx-5 p-2 rounded-lg shadow-lg">
+          <div>
+            <vue-numeric-input
+              v-model="numOfQuestion"
+              :step="1"
+            ></vue-numeric-input>
+          </div>
+        </div>
       </div>
       <div class="flex flex-row justify-between items-center w-full h-15 my-5">
         <h2 class="text-white w-1/4 mx-5">Question Type:</h2>
         <div class="flex flex-row justify-between w-2/3 h-auto mx-5 p-2">
           <div class="flex items-center mx-2 w-auto">
-            <div class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white">
+            <div
+              class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white"
+            >
               <p class="select-none m-2">Multiple Choice</p>
             </div>
           </div>
           <div class="flex items-center mx-2 w-auto">
-            <div class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white">
+            <div
+              class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white"
+            >
               <p class="select-none m-2">Short Answer</p>
             </div>
           </div>
           <div class="flex items-center mx-2 w-auto">
-            <div class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white">
+            <div
+              class="bg-green2 rounded-lg shadow-lg hover:bg-red2 hover:text-white"
+            >
               <p class="select-none m-2">Long Answer</p>
             </div>
           </div>
@@ -35,19 +67,36 @@
       </div>
     </div>
     <div class="flex justify-between mt-auto">
-      <div class="w-10">Button</div>
+      <button class="bg-red2 rounded-lg shadow-lg py-2 px-4">Start Quiz</button>
     </div>
   </ContentBox>
 </template>
 
 <script>
+
+import Multiselect from 'vue-multiselect';
+import VueNumericInput from 'vue-numeric-input';
 import ContentBox from '~/components/Quiz/ContentBox.vue';
 
 export default {
-  name: 'beginquiz',
-  layout: 'quizbg',
+  name: 'BeginQuiz',
+
   components: {
-    ContentBox
-  }
+    ContentBox,
+    Multiselect,
+    VueNumericInput
+  },
+  layout: 'quizbg',
+  data: () => ({
+    numOfQuestion: 0,
+    subjectValue: '',
+    topicValue: [],
+    subjectOptions: ['Math', 'Geography', 'Physic'],
+    topicOptions: ['Calculus', 'Trigonometry', 'Logarithm'],
+  }),
+  methods: {
+  },
 };
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
