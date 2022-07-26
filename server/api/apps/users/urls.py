2959d7b5ada9d18748a926897019799065bbad70
yasawiharja <yasapurwasamudra@gmail.com>
2022-07-26 10:20:55 +0800

    feat:sends data to the front-end

--- server/api/apps/users/urls.py
+++ server/api/apps/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import UserViewSet, UserStatistics
 
 router = routers.DefaultRouter()
 router.register(r"", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("<int:pk>", views.UserStatistics.as_view(), name = 'user-statistics' )
 ]
--- server/api/apps/users/views.py
+++ server/api/apps/users/views.py
@@ -1,15 +1,20 @@
+from tkinter.messagebox import QUESTION
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
-from shared_models.models.statistics_models import QuizStatistics
-from shared_models.models.quiz_models import Question, Tag, Answer
+from shared_models.models.statistics_models import QuestionResponse
+from shared_models.models.quiz_models import Question
 from django.views import generic
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 class UserStatistics(generic.ListView):
-    model = QuizStatistics
+    model = QuestionResponse, Question.QuestionType
+    queryset = QuestionResponse.objects.filter(user__exact='request.user'), Question.QuestionType.objects.filter(creator__exact='request.creator')
+    context_object_name = 'user_answered_question_and_made_quizzes'
+    #template_name: template_name/location
+    
     
     
     
