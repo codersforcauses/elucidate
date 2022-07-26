d975866d2ac643e54fd451bb404a19fa6dc57748
yasawiharja <yasapurwasamudra@gmail.com>
2022-07-26 10:17:51 +0800

    feat: sends data to the front-end

--- server/api/apps/users/views.py
+++ server/api/apps/users/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
-
+from shared_models.models.statistics_models import QuizStatistics
+from shared_models.models.quiz_models import Question, Tag, Answer
+from django.views import generic
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+class UserStatistics(generic.ListView):
+    model = QuizStatistics
+    
+    
+    
+
