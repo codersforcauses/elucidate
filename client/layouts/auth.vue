6a7f0fdad64efe2867d7806672fffee7fba78a73
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-07-27 16:08:58 +0800

    Merge AuthHeader & AuthFooter into auth layout

--- client/layouts/auth.vue
+++ client/layouts/auth.vue
@@ -1,10 +1,17 @@
 <template>
   <div
-    class="flex flex-col items-center content-center min-w-full min-h-screen"
+    class="flex flex-col items-center content-center min-w-full min-h-screen p-5"
   >
-    <AuthHeader :page-title="title" />
+    <nuxt-img src="branding/logo.svg" class="w-16" alt="Elucidate Logo" />
+    <div class="flex items-center my-5 mr-10">
+      <font-awesome-layers class="fa-2x mx-3">
+        <font-awesome-icon :icon="['fas', 'circle']" class="text-green" />
+        <font-awesome-icon :icon="['fas', 'user']" class="text-lg text-white" />
+      </font-awesome-layers>
+      <p class="text-3xl italic">{{ title }}</p>
+    </div>
     <Nuxt />
-    <AuthFooter />
+    <nuxt-img src="shapes.svg" class="w-72 mt-auto" alt="Shapes" />
   </div>
 </template>
 
