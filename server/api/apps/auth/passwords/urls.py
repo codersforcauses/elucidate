a918ca645d9cb32a93855c506764b0161e9d9291
root <yvesmiguelreyes1@gmail.com>
2022-07-27 09:45:21 +0000

    Elucidate SMTP Gmail Setup

--- server/api/apps/auth/passwords/urls.py
+++ server/api/apps/auth/passwords/urls.py
@@ -7,6 +7,6 @@ from .views import PasswordReset
 urlpatterns = [
     # path("", views.PasswordResetView.as_view()),
     path("", PasswordReset.as_view()),
-    path("<str:token>/", ChangePasswordView.as_view(),  name = "reset-password" )
+    path("<str:token>/", ChangePasswordView.as_view(), name = "reset-password" )
 
 ]
