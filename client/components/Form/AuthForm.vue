b0288a777e293846d453f19cec4bfae11a015919
David <david@gridcognition.com>
2022-07-30 18:04:41 +0800

    Add password reset forms

--- client/components/Form/AuthForm.vue
+++ client/components/Form/AuthForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center w-11/12 max-w-lg bg-green drop-shadow-lg"
+    class="flex flex-col items-center pb-5 mb-10 rounded w-96 pt-11 px-9 bg-green1 drop-shadow-lg"
   >
     <slot></slot>
   </div>
