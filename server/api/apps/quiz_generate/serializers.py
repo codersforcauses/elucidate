f5952d2694c73d80ea1d00df9baa0ee1bb09a87d
Jack Sun <jsun1590@gmail.com>
2022-12-14 16:29:03 +0000

    generate quiz

--- server/api/apps/quiz_generate/serializers.py
+++ server/api/apps/quiz_generate/serializers.py
@@ -4,15 +4,16 @@ from api.apps.shared_models.models.quiz_models import Subject, Topic
 
 
 class GenerateQuizRequestSerializer(serializers.Serializer):
-    subject = serializers.PrimaryKeyRelatedField(queryset=Subject.objects.all())
+    subject = serializers.PrimaryKeyRelatedField(
+        queryset=Subject.objects.all()
+    )
     topics = serializers.PrimaryKeyRelatedField(
         queryset=Topic.objects.all(), many=True
     )
     question_count = serializers.IntegerField(min_value=1)
+    question_types = serializers.DictField(
+        child=serializers.BooleanField(),
+    )
 
     class Meta:
-        fields = [
-            "subject",
-            "topics",
-            "question_count",
-        ]
+        fields = ["subject", "topics", "question_count", "question_types"]
--- server/api/apps/quiz_generate/urls.py
+++ server/api/apps/quiz_generate/urls.py
@@ -5,18 +5,17 @@ from . import views
 app_name = "quiz_generate"
 urlpatterns = [
     path(
-        "generate",
+        "generate/",
         views.GenerateQuizView.as_view(),
-        name="generate-quiz",
     ),
-    path(
-        "subject-exists",
-        views.SubjectExistsView.as_view(),
-        name="subject-exists",
-    ),
-    path(
-        "topic-exists",
-        views.TopicExistsView.as_view(),
-        name="topic-exists",
-    ),
+    # path(
+    #     "subject-exists",
+    #     views.SubjectExistsView.as_view(),
+    #     name="subject-exists",
+    # ),
+    # path(
+    #     "topic-exists",
+    #     views.TopicExistsView.as_view(),
+    #     name="topic-exists",
+    # ),
 ]
